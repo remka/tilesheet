b2009f32d65c7ce7359e9b3a643683bf671521ce
remka <remka@users.noreply.github.com>
2019-02-14 18:23:09 +0900

    Still buggy...

--- tile.js
+++ tile.js
@@ -10,10 +10,19 @@ var questions = [{
 inquirer.prompt(questions).then(answers => {
 
   var imagePath,
-      tileSize;
+      tileSize,
+      iHeight,
+      iWidth,
+      tilesH,
+      tilesW,
+      numTiles;
+
+  var tileNum = 0;
 
   var tilesArray = [],
       newArray = [];
+
+  var isSame = true;
 
   var imagePath = process.argv[2];
   if (answers['tileSize'] == '') {
@@ -26,11 +35,12 @@ inquirer.prompt(questions).then(answers => {
 
   Jimp.read(imagePath, (err, image) => {
     if (err) throw err;
-    var iHeight = image.bitmap.height;
-    var iWidth = image.bitmap.width;
-    var tilesH = iHeight / tileSize;
-    var tilesW = iWidth / tileSize;
-    console.log(`Current image is a ${tilesW}x${tilesH} tiles grid.`);
+    iHeight = image.bitmap.height;
+    iWidth = image.bitmap.width;
+    tilesH = iHeight / tileSize;
+    tilesW = iWidth / tileSize;
+    numTiles = tilesH * tilesW;
+    console.log('\x1b[42m%s\x1b[0m',`Current image is a ${tilesW}x${tilesH} tiles grid (${numTiles} tiles).`);
     // looping all tiles
     for (var i=0; i<tilesW; i++) {  // for (var i=0; i<tilesW; i++) {
       for (var j=0; j<tilesH; j++) {  // for (var j=0; j<tilesH; j++) {
@@ -42,41 +52,86 @@ inquirer.prompt(questions).then(answers => {
             var currX = l + (tileSize * i);
             //console.log('Pixel [' + currY + ', ' + currX + ']');
             var color = image.getPixelColor(currX, currY);
-            var hex = Jimp.intToRGBA(color);
-            tilePixels[k][l] = hex;
+            //var hex = Jimp.intToRGBA(color);
+            tilePixels[k][l] = color;
           }
         }
-        console.log('Tile [' + j + ', ' + i + ']');
-        console.log(tilePixels);
+        //console.log('Tile [' + j + ', ' + i + ']');
+        //console.log(tilePixels);
+
         // if tilesArray is empty, add the first tile in it.
-        /*
         if (tilesArray.length == 0){
+          //console.log('Adding the first tile in the array.');
+          //console.log('-------');
           tilesArray.push(tilePixels);
         } else {
           // Not empty, we need to compare all entries in the tilesArray Array
           // with the current tilePixels array.
-          var isSame = true;
-          var currLength = tilesArray.length;
-          for (var i=0; i<currLength; i++) {
-            for (var k=0; k<tileSize; k++) {
-              for (var l=0; l<tileSize; l++) {
-                if(tilesArray[i][k][l] != tilePixels[k][l]) {
+          tileNum += 1;
+
+          //console.log('Currently ' + tilesArray.length + ' tiles in array.');
+          for (var m=0; m<tilesArray.length; m++) {
+            //console.log('Comparing tile #' + tileNum + ' with tile #' + m + '...');
+            isSame = true;
+            for (var n=0; n<tileSize; n++) {
+              for (var o=0; o<tileSize; o++) {
+                if (tilesArray[m][n][o] !=  tilePixels[n][o]) {
                   isSame = false;
                 }
               }
             }
           }
-          console.log(isSame);
-          if (isSame == false) {
+          if (!isSame) {
+            //console.log('\x1b[42m%s\x1b[0m','Add curent tile to tilesArray.');
             tilesArray.push(tilePixels);
           }
+          //console.log('-------');
+          //console.log(isSame);
         }
-        */
+
       }
     }
-    //var col = image.getPixelColor(10, 25); // returns the colour of that pixel e.g. 0xFFFFFFFF
-    //var hex = Jimp.intToRGBA(col);
-    //console.log(hex);
+
     console.log('New image has ' + tilesArray.length + ' tiles.');
+    //console.log(tilesArray[0]);
+
+    var imgW = tilesArray.length * 16;
+    let newImg = new Jimp(imgW, 16, function (err, newImg) {
+      if (err) throw err;
+
+      //console.log(tilesArray[0][0][0]);
+
+      for (var i = 0; i<tilesArray.length; i++) {
+
+        for (var j=0; j<tileSize; j++) {
+          for (var k=0; k<tileSize; k++) {
+            //console.log(tilesArray[i][j][k]);
+            var currColor = tilesArray[i][j][k];
+            var x = k + (tileSize * i);
+            var y = j;
+            newImg.setPixelColor(currColor, x, y);
+          }
+        }
+      }
+
+      newImg.write('test.png', (err) => {
+        if (err) throw err;
+      });
+
+      /*
+
+      imageData.forEach((row, y) => {
+        row.forEach((color, x) => {
+          image.setPixelColor(color, x, y);
+        });
+      });
+
+      image.write('test.png', (err) => {
+        if (err) throw err;
+      });
+
+      */
+    });
+
   });
 })
