0b825252cfc8a105736482b45568b97b5780018c
remka <remka@users.noreply.github.com>
2019-02-15 11:35:12 +0900

    Minor changes

--- README.md
+++ README.md
@@ -14,3 +14,5 @@ Call it with `node tile <path-to-image>`. The test image should work with `node 
 A prompt asks you the size of your tiles. Type the size in px (just the number), or leave it blank to use the default valu (16px, but easy to change by editing the script). The script only supports quare tiles for now.  
 
 The script will create another image in the same folder, identical *minus* duplicated tiles.
+
+The level.png image was created by [Clint Bellanger](https://opengameart.org/forumtopic/feedback-on-16px-robots-and-tiles) ans used only for illustration purpose.  
--- tile.js
+++ tile.js
@@ -15,20 +15,27 @@ inquirer.prompt(questions).then(answers => {
       iWidth,
       tilesH,
       tilesW,
-      numTiles;
+      numTiles,
+      newName;
 
   var tileNum = 0;
   var tilesArray = [];
   var isSame = true;
 
   var imagePath = process.argv[2];
+
+  // getting tile size
   if (answers['tileSize'] == '') {
     tileSize = 16;
   } else {
     tileSize = parseInt(answers['tileSize']);
   }
 
-  console.log(`Working with image: ${imagePath} and a ${tileSize}x${tileSize} grid.`);
+  // name of new file
+  newName = imagePath.split('.');
+  newName = newName[0] + '-tile.png';
+
+  //console.log(`Working with image: ${imagePath} and a ${tileSize}x${tileSize} grid.`);
 
   Jimp.read(imagePath, (err, image) => {
     if (err) throw err;
@@ -37,68 +44,72 @@ inquirer.prompt(questions).then(answers => {
     tilesH = iHeight / tileSize;
     tilesW = iWidth / tileSize;
     numTiles = tilesH * tilesW;
-    console.log('\x1b[42m%s\x1b[0m',`Current image is a ${tilesW}x${tilesH} tiles grid (${numTiles} tiles).`);
-    // looping all tiles
-    for (var i=0; i<tilesW; i++) {
-      for (var j=0; j<tilesH; j++) {
-        var tilePixels = new Array(tileSize).fill('').map(() => new Array(tileSize).fill(''));
-        for (var k=0; k<tileSize; k++) {
-          for (var l=0; l<tileSize; l++) {
-            var currY = k + (tileSize * j);
-            var currX = l + (tileSize * i);
-            var color = image.getPixelColor(currX, currY);
-            //var hex = Jimp.intToRGBA(color);
-            tilePixels[k][l] = color;
+    if(iHeight % tileSize !== 0 || iWidth % tileSize !== 0) {
+      console.log(`Image dimensions (${iWidth}x${iHeight}px) are not divisible by tile size (${tileSize}px).`);
+    } else {
+      console.log(`Current image is a ${tilesW}x${tilesH} tiles grid (${numTiles} tiles).`);
+      // looping all tiles
+      for (var i=0; i<tilesW; i++) {
+        for (var j=0; j<tilesH; j++) {
+          var tilePixels = new Array(tileSize).fill('').map(() => new Array(tileSize).fill(''));
+          for (var k=0; k<tileSize; k++) {
+            for (var l=0; l<tileSize; l++) {
+              var currY = k + (tileSize * j);
+              var currX = l + (tileSize * i);
+              var color = image.getPixelColor(currX, currY);
+              //var hex = Jimp.intToRGBA(color);
+              tilePixels[k][l] = color;
+            }
           }
-        }
 
-        // if tilesArray is empty, add the first tile in it.
-        if (tilesArray.length == 0){
-          tilesArray.push(tilePixels);
-        } else {
-          // Not empty, we need to compare all entries in the tilesArray Array
-          // with the current tilePixels array.
-          tileNum += 1;
-          for (var m=0; m<tilesArray.length; m++) {
-            isSame = true;
-            for (var n=0; n<tileSize; n++) {
-              for (var o=0; o<tileSize; o++) {
-                if (tilesArray[m][n][o] !=  tilePixels[n][o]) {
-                  isSame = false;
+          // if tilesArray is empty, add the first tile in it.
+          if (tilesArray.length == 0){
+            tilesArray.push(tilePixels);
+          } else {
+            // Not empty, we need to compare all entries in the tilesArray Array
+            // with the current tilePixels array.
+            tileNum += 1;
+            for (var m=0; m<tilesArray.length; m++) {
+              isSame = true;
+              for (var n=0; n<tileSize; n++) {
+                for (var o=0; o<tileSize; o++) {
+                  if (tilesArray[m][n][o] !=  tilePixels[n][o]) {
+                    isSame = false;
+                  }
                 }
               }
+              if (isSame) break;
             }
-            if (isSame) break;
+            if (!isSame) {
+              tilesArray.push(tilePixels);
+            }
           }
-          if (!isSame) {
-            tilesArray.push(tilePixels);
-          }
-        }
 
-      }
-    }
-
-    console.log('New image has ' + tilesArray.length + ' tiles.');
-
-    var imgW = tilesArray.length * tileSize;
-    let newImg = new Jimp(imgW, tileSize, function (err, newImg) {
-      if (err) throw err;
-      for (var i = 0; i<tilesArray.length; i++) {
-        for (var j=0; j<tileSize; j++) {
-          for (var k=0; k<tileSize; k++) {
-            var currColor = tilesArray[i][j][k];
-            var x = k + (tileSize * i);
-            var y = j;
-            newImg.setPixelColor(currColor, x, y);
-          }
         }
       }
-      var newName = imagePath.split('.');
-      newName = newName[0] + '-tile.png';
-      newImg.write(newName, (err) => {
+
+      var pct = Math.floor((numTiles * 100)/tilesArray.length);
+      console.log('New image has ' + tilesArray.length + ' tiles.' + pct);
+
+      var imgW = tilesArray.length * tileSize;
+      let newImg = new Jimp(imgW, tileSize, function (err, newImg) {
         if (err) throw err;
+        for (var i = 0; i<tilesArray.length; i++) {
+          for (var j=0; j<tileSize; j++) {
+            for (var k=0; k<tileSize; k++) {
+              var currColor = tilesArray[i][j][k];
+              var x = k + (tileSize * i);
+              var y = j;
+              newImg.setPixelColor(currColor, x, y);
+            }
+          }
+        }
+        var newName = imagePath.split('.');
+        newName = newName[0] + '-tile.png';
+        newImg.write(newName, (err) => {
+          if (err) throw err;
+        });
       });
-    });
-
+    }
   });
 })
